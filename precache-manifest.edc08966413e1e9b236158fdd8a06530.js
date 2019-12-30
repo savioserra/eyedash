ad362d545983cc4e8c9504fd09b2bf8e15b4973c
Sávio Serra <serra.santos.s@gmail.com>
2019-12-30 01:45:38 -0200

    Updates

---
+++ precache-manifest.edc08966413e1e9b236158fdd8a06530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d398478f83e76f6099027be74b2aad4e",
+    "url": "/eyedash/index.html"
+  },
+  {
+    "revision": "e88dd8b98fb234e0ae02",
+    "url": "/eyedash/static/css/2.f7fa3ae5.chunk.css"
+  },
+  {
+    "revision": "e88dd8b98fb234e0ae02",
+    "url": "/eyedash/static/js/2.f1fd1213.chunk.js"
+  },
+  {
+    "revision": "335932bcfe373407d7c8acc8ad15eddd",
+    "url": "/eyedash/static/js/2.f1fd1213.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7c7840aa0ae46d73f75b",
+    "url": "/eyedash/static/js/main.24a8bd9c.chunk.js"
+  },
+  {
+    "revision": "362afecb393f242a93cb",
+    "url": "/eyedash/static/js/runtime-main.132aae2b.js"
+  }
+]);
